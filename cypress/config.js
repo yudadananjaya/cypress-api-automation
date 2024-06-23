852cf27ba5cc7da57d53fbd1558bc119a8a14eff
unknown <yudananjayaii@gmail.com>
2024-06-23 15:00:17 +0700

    Set up Cypress with mochawesome reporter and CircleCI integration

--- cypress/config.js
+++ cypress/config.js
@@ -1,3 +1,11 @@
 module.exports = {
    baseUrl: 'https://reqres.in/api/login',
+   reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      reportFilename: 'report',
+      overwrite: false,
+      html: true,
+      json: true
+    }
 };
