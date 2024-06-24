3f9aa58f9c0b82f476ab68a53ccf339e1d91bdbf
unknown <yudananjayaii@gmail.com>
2024-06-24 18:46:08 +0700

    Set up Cypress CircleCI integration

--- cypress.config.js
+++ cypress.config.js
@@ -1,30 +1,24 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000', // Set the base URL for your tests
-    viewportWidth: 1280, // Set the default viewport width
-    viewportHeight: 720, // Set the default viewport height
+    baseUrl: 'http://localhost:3000',
+    viewportWidth: 1280,
+    viewportHeight: 720,
     setupNodeEvents(on, config) {
       // Implement node event listeners here
-      
-      // Example: Log test names
       on('test:before:run', (details) => {
         console.log('Running test:', details.title);
       });
 
-      // Example: Extend browser launch options
       on('before:browser:launch', (browser = {}, launchOptions) => {
         if (browser.name === 'chrome' && browser.isHeadless) {
           launchOptions.args.push('--disable-gpu');
         }
-
         return launchOptions;
       });
 
-      // Return the updated config object
       return config;
     },
   },
-  // Other Cypress configurations can be added here
 });
