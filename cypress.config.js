e21c76c5af1737f932be3a887461b68128e054fc
Galih J.A <galihj.a@Galihs-MacBook-Pro.local>
2024-07-04 14:39:13 +0800

    fix package

--- cypress.config.js
+++ cypress.config.js
@@ -1,24 +1,10 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
-    viewportWidth: 1280,
-    viewportHeight: 720,
     setupNodeEvents(on, config) {
-      // Implement node event listeners here
-      on('test:before:run', (details) => {
-        console.log('Running test:', details.title);
-      });
-
-      on('before:browser:launch', (browser = {}, launchOptions) => {
-        if (browser.name === 'chrome' && browser.isHeadless) {
-          launchOptions.args.push('--disable-gpu');
-        }
-        return launchOptions;
-      });
-
-      return config;
+      // implement node event listeners here
     },
+    baseUrl: "https://reqres.in/api/login",
   },
 });
---
+++ run_cypress.sh
@@ -0,0 +1,8 @@
+# run test in headless mode
+npx cypress run 
+
+# run test in headed mode
+#npx cypress run --headed
+
+# run test with cypress GUI
+#npx cypress open
